7c6a42d16f6654a004d5cb3dd4f337cc453e5cea
ianmagdaleno <ianmagdaleno1726@gmail.com>
2024-01-11 09:49:11 -0300

    Updated player movement with joystick input, blend tree for animations and cinemachine to follow player

--- HyperCasualTest/Assets/Scripts/PlayerController.cs
+++ HyperCasualTest/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private InputActionReference joystickToMove;
+    [SerializeField] private float speed;
+    [SerializeField] private GameObject player;
+    [SerializeField] private Animator animator;
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector2 moveDir = joystickToMove.action.ReadValue<Vector2>();
+
+        if (moveDir != Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(moveDir.x, moveDir.y) * Mathf.Rad2Deg;
+
+            player.transform.rotation = Quaternion.Euler(0f, targetRotation, 0f);
+            player.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            animator.SetFloat("speed", moveDir.magnitude);
+        }
+        else
+        {
+            animator.SetFloat("speed", 0f);
+        }
     }
 }
